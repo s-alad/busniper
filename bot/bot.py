87e86aae2ec89dbe2ba7c799d949f953e6ceb3bd
onkr0d <90716666+onkr0d@users.noreply.github.com>
2023-04-19 12:07:31 -0400

    massive memory optimizations + refactor

--- bot/bot.py
+++ bot/bot.py
@@ -1,29 +1,25 @@
-from selenium import webdriver
-
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.wait import WebDriverWait
-
-from bs4 import BeautifulSoup
-import time
-import pickle
+import collections
 import os
+import pickle
+import time
+
 import requests
-import collections ; collections.Callable = collections.abc.Callable
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+collections.Callable = collections.abc.Callable
 
 from course import Course, Section
 
 from dotenv import load_dotenv
+
 
 class Sniper:
     load_dotenv()
@@ -36,9 +32,12 @@ class Sniper:
 
     def __init__(self):
         options = ChromeOptions()
-        options.add_experimental_option("detach", True) #add expirimental option to keep window open after test is done for debugging
-        options.add_argument("user-data-dir={}".format(self.path+"/profile"))
+        options.add_experimental_option("detach",
+                                        True)  # add expirimental option to keep window open after test is done for debugging
+        options.add_argument("user-data-dir={}".format(self.path + "/profile"))
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        self.wait_10 = WebDriverWait(self.driver, 10)
+        self.wait_30 = WebDriverWait(self.driver, 30)
 
     def headers(self):
         return {
@@ -62,12 +61,12 @@ class Sniper:
 
     def getCookies(self):
         driver = self.driver
-        #wait until url changes to https://student.bu.edu/MyBU/s/ to save cookies
-        #WebDriverWait(driver, 30).until(EC.url_changes(self.entry))
-        #finish = WebDriverWait(driver, 30).until(EC.presence_of_element_located(((By.CLASS_NAME, "community_navigation-tileMenuItemBanner_tileMenuItemBanner"))))
-        #save cookies to cookies.pkl
-        #pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
-        #for cookie in driver.get_cookies(): print(cookie)
+        # wait until url changes to https://student.bu.edu/MyBU/s/ to save cookies
+        # WebDriverWait(driver, 30).until(EC.url_changes(self.entry))
+        # finish = WebDriverWait(driver, 30).until(EC.presence_of_element_located(((By.CLASS_NAME, "community_navigation-tileMenuItemBanner_tileMenuItemBanner"))))
+        # save cookies to cookies.pkl
+        # pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
+        # for cookie in driver.get_cookies(): print(cookie)
 
         driver.get("https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1?ModuleName=menu.pl&NewMenu=Academics")
         time.sleep(1)
@@ -82,37 +81,43 @@ class Sniper:
         driver.get(self.entry)
         print("Logging in...")
         try:
-            user = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "j_username"))).send_keys(self.username)
-            pasw = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "j_password"))).send_keys(self.password)
-            button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "_eventId_proceed"))).click()
+            self.wait_10.until(EC.presence_of_element_located((By.ID, "j_username"))).send_keys(
+                self.username)
+            self.wait_10.until(EC.presence_of_element_located((By.ID, "j_password"))).send_keys(
+                self.password)
+            self.wait_10.until(
+                EC.presence_of_element_located((By.NAME, "_eventId_proceed"))).click()
 
-            WebDriverWait(driver, 30).until(EC.url_changes("https://shib.bu.edu/idp/profile/SAML2/POST-SimpleSign"))
+            self.wait_30.until(EC.url_changes("https://shib.bu.edu/idp/profile/SAML2/POST-SimpleSign"))
             print("switched to shibboleth")
 
-            #check if url is now https://student.bu.edu/MyBU/s/ otherwise go to duo() function
+            # check if url is now https://student.bu.edu/MyBU/s/ otherwise go to duo() function
             time.sleep(5)
             if driver.current_url == "https://student.bu.edu/MyBU/s/":
                 print("DUO NOT NEEDED")
                 pass
-            else: self.duo()
+            else:
+                self.duo()
             print("Login successful")
         except TimeoutException:
             print("Timed out waiting for page to load")
             driver.quit()
-        
-        finish = WebDriverWait(driver, 30).until(EC.presence_of_element_located(((By.CLASS_NAME, "community_navigation-tileMenuItemBanner_tileMenuItemBanner"))))
+
+        finish = WebDriverWait(driver, 30).until(EC.presence_of_element_located(
+            ((By.CLASS_NAME, "community_navigation-tileMenuItemBanner_tileMenuItemBanner"))))
 
     def duo(self):
         driver = self.driver
         try:
-            frame = WebDriverWait(driver, 30).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "duo_iframe")))
-            save = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.NAME, "dampen_choice"))).click()
-            passcode = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "passcode")))
-            auth_buttons = WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "auth-button")))
+            self.wait_30.until(EC.frame_to_be_available_and_switch_to_it((By.ID, "duo_iframe")))
+            self.wait_30.until(EC.presence_of_element_located((By.NAME, "dampen_choice"))).click()
+            self.wait_30.until(EC.presence_of_element_located((By.ID, "passcode")))
+            auth_buttons = self.wait_30.until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "auth-button")))
             push = auth_buttons[0].click()
         except Exception as e:
             print(e)
-    
+
     def cookieLogin(self):
         driver = self.driver
         if os.path.exists("cookies.pkl"):
@@ -126,12 +131,14 @@ class Sniper:
             print("Cookies do not exist, logging in...")
             self.login()
             self.getCookies()
-    
+
     def register(self, course: Course):
         driver = self.driver
-        #panel = WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "community_navigation-tileMenuItem_tileMenuItem")))
-        self.driver.get("https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1?ModuleName=reg/option/_start.pl&ViewSem=Fall%202023&KeySem=20243")
-        self.driver.get("https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1?ModuleName=reg%2Fadd%2F_start.pl&ViewSem=Fall%202023&KeySem=20243")
+        # panel = WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "community_navigation-tileMenuItem_tileMenuItem")))
+        self.driver.get(
+            "https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1?ModuleName=reg/option/_start.pl&ViewSem=Fall%202023&KeySem=20243")
+        self.driver.get(
+            "https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1?ModuleName=reg%2Fadd%2F_start.pl&ViewSem=Fall%202023&KeySem=20243")
 
         uri = "https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1?ModuleName=reg%2Fadd%2Fbrowse_schedule.pl&SearchOptionDesc=Class+Number&SearchOptionCd=S&ViewSem=Fall+2023&KeySem=20243&AddPlannerInd=&College={}&Dept={}&Course={}&Section={}".format(
             course.college,
@@ -142,12 +149,12 @@ class Sniper:
         self.driver.get(uri)
         time.sleep(1)
         self.snipe(uri)
-    
+
     def snipe(self, uri):
         r = requests.get(uri, headers=self.headers())
         print(r.status_code)
         soup = BeautifulSoup(r.content, 'html5lib')
-        form = soup.find('form', attrs = {'name': 'SelectForm'})
+        form = soup.find('form', attrs={'name': 'SelectForm'})
         table = form.find('table')
         trs = table.find_all('tr')[3:]
 
@@ -156,20 +163,21 @@ class Sniper:
             mark = tds[0].text
 
             section = Section(
-                marktoadd=mark, 
-                classname=tds[2].text, 
-                titleinstructor=tds[3].text, 
-                openseats=tds[5].text, 
-                credithours=tds[6].text, 
-                classtype=tds[7].text, 
-                building=tds[8].text, 
-                room=tds[9].text, 
-                day=tds[10].text, 
-                start=tds[11].text, 
-                stop=tds[12].text, 
+                marktoadd=mark,
+                classname=tds[2].text,
+                titleinstructor=tds[3].text,
+                openseats=tds[5].text,
+                credithours=tds[6].text,
+                classtype=tds[7].text,
+                building=tds[8].text,
+                room=tds[9].text,
+                day=tds[10].text,
+                start=tds[11].text,
+                stop=tds[12].text,
                 notes=tds[13].text)
-            
-            print(section, ">" ,section.can_add())
+
+            print(section, ">", section.can_add())
+
 
 if __name__ == "__main__":
     bot = Sniper()
