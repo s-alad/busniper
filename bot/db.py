262d8e0e4f58cd0c62a8b9628d24ddfeacfa28ac
s-alad <n7.saad@gmail.com>
2023-06-02 22:45:06 -0400

    db clean

--- bot/db.py
+++ bot/db.py
@@ -9,43 +9,37 @@ from models.ping import Ping
 from models.section import Section
 from models.course import Course
 
-dotenv.load_dotenv()
+class Database:
 
-redispassword = os.getenv('REDISPASSWORD') #os.getenv('RAILWAYREDIS') 
-red = redis.Redis(
-    host='redis-18876.c232.us-east-1-2.ec2.cloud.redislabs.com',
-    port=18876,
-    password=redispassword
-) 
+    dotenv.load_dotenv()
+    redispassword = os.getenv('REDISPASSWORD') #os.getenv('RAILWAYREDIS') 
+    red = redis.Redis(
+        host='redis-18876.c232.us-east-1-2.ec2.cloud.redislabs.com',
+        port=18876,
+        password=redispassword
+    ) 
 
-#generally takes .2s to add and .07s to fetch
-def get_pings():
-    return red.json().get("Pings")
+    def __init__(self):
+        pass
 
-def add_ping(ping: Ping):
-    return red.json().arrappend("Pings", "$", ping)
+    def get_pings(self):
+        return self.red.json().get("Pings")
 
-def get_all_courses():
-    c = red.json().get("Courses")
-    print(c)
+    def add_ping(self, ping: Ping):
+        return self.red.json().arrappend("Pings", "$", ping)
 
-def get_course(course: Course):
-    return red.json().get("Courses", Path(str(course)), no_escape=True)
+    def get_all_courses(self):
+        return self.red.json().get("Courses")
+        
+    def get_course(self, course: Course):
+        return self.red.json().get("Courses", Path(str(course)), no_escape=True)
 
-def add_course(course: Course):
-    addition = {"subscribers" : []}
-    return red.json().set("Courses", Path(str(course)), addition)
+    def add_course(self, course: Course):
+        return self.red.json().set("Courses", Path(str(course)), {"subscribers" : []})
 
-def add_subscriber(course: Course, email: str):
-    return red.json().arrappend("Courses", Path(str(course)+".subscribers"), email)
+    def add_subscriber(self, course: Course, email: str):
+        return self.red.json().arrappend("Courses", Path(str(course)+".subscribers"), email)
 
-""" start = time.time()
-print(get_course(Course("CAS", "CS", "237", "A1")))
-end = time.time()
-print(end - start) """
-
-#print(add_course(Course("CAS", "CS", "112", "A1")))
-#print(add_subscriber(Course("CAS", "CS", "112", "A1"), "lol@lol.com"))
-
-#print(get_pings())
-#print(add_ping(Ping("test.com", Course("CAS", "CS", "112", "A1")).__dict__()))
+if __name__ == "__main__":
+    db = Database()
+    print(db.get_all_courses())
